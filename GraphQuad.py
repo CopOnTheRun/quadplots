75641bd1d787a7b1e71d9eb1fce5f7a20e8f737b
Chris Copley <CopOnTheRun@gmail.com>
2021-11-26 19:58:22 -0500

    Tries to make the code work
    A lot changed here, but basically I split off the animated portion of
    the Graph class into a separate class that inherits from it. Also tried
    to make the graph class more like a wrapper for figure by allowing *args
    and **kwargs to be passed through.

--- GraphQuad.py
+++ GraphQuad.py
@@ -1,45 +1,53 @@
 import matplotlib
 from matplotlib import pyplot
+from matplotlib.animation import FuncAnimation
 import numpy as np
-from typing import Sequence, Optional, Iterable
+import sys
+from typing import Sequence, Optional, Iterable, Sized
 
 from Quadrature import Quadrature
 
 class Graph:
-    def __init__(self, quads: Sequence[Quadrature],
-            layout: tuple[int,int], style: str = "seaborn"):
+    def __init__(self, quads: Sequence[Quadrature], layout: tuple[int,int],
+            error: bool = True, **kwargs):
         self.quads = quads
-        self.style = style
         self.layout = layout
-        self.fig, self.axes = self.background()
+        self.fig, self.quad_axes, self.error_axes = self.create_subplots(error,**kwargs)
         self.colors = pyplot.rcParams['axes.prop_cycle'].by_key()['color']
+        self.curve = self.get_curve()
+        self.error = self.get_error()
+        self.points = self.get_points()
+        self.shapes = self.get_shapes()
+        self.title = self.get_title()
+        self.fig.tight_layout()
 
-    def animate(self, size:int):
-        for ax in self.axes:
-            ax.clear()
-
-        for quad in self.quads:
-            quad.interval //= size
-
-        self.points()
-        self.curve()
-        return self.quadrature()
-
-    def background(self):
+    def create_subplots(self, error: bool, **kwargs):
         """Return and possibly write to a file, a graphic representation of the Riemann sum"""
         #setting up matplotlib
-        pyplot.style.use(self.style)
         #matplotlib.rcParams['text.usetex'] = True
 
-        #creating the figure
-        fig = pyplot.figure()
         num_quads = len(self.quads)
+        spots = self.layout[0] * self.layout[1]
+
+        #int cast unecessary because bool is a subclass of int, but clarity
+        if num_quads + int(error) > spots:
+            raise ValueError("Not enough room for all the subplots.")
+        fig = pyplot.figure(**kwargs)
+
+        quad_axes = []
+
+        #each quadrature subplot
         for i in range(1,num_quads+1):
-            fig.add_subplot(*self.layout,i)
+            quad_axes.append(fig.add_subplot(*self.layout,i))
 
-        return fig, fig.axes
+        error_axes = None
+        if error:
+            error_spots = (num_quads + 1, spots)
+            error_axes = fig.add_subplot(*self.layout, error_spots)
 
-    def curve(self):
+        return fig, quad_axes, error_axes
+
+    def get_curve(self):
         #this makes it so that the function curve goes past the bounds of the interval. Purely asthetics.
         quad = self.quads[0]
         overshoot = .025*abs(quad.interval.length)
@@ -50,50 +58,99 @@ class Graph:
         x = np.linspace(start, end, 200)
         y = quad.func.func(x)
         label = f"$y = {quad.func.string}$" if quad.func.string else "$y=f(x)$"
-        for ax in self.axes:
+        lines = []
+        for ax in self.quad_axes:
             line, = ax.plot(x,y,color="black")
             line.set_label(label)
+            lines.append(line)
+        return lines
 
     def legend(self, axes_legend = True):
         if axes_legend:
-            for ax in self.axes:
+            for ax in self.quad_axes:
                 ax.legend()
 
         if not axes_legend:
-            handles, labels = self.axes[0].get_legend_handles_labels()
+            handles, labels = self.quad_axes[0].get_legend_handles_labels()
             self.fig.legend(handles, labels)
 
-    def title(self):
+    def get_title(self):
         title_str = f"Quadrature approximation of ${self.quads[0].func.string}$"
         return self.fig.suptitle(title_str)
 
-    def error(self):
-        if len(self.quads) >= self.layout[0]*self.layout[1]:
-            raise ValueError("Need a place to put the error graph.")
-        er_ax = self.fig.add_subplot(*self.layout,len(self.axes)+1)
+    def get_error(self):
+        er_ax = self.error_axes
         er_ax.axhline(color="black",lw=.5)
         colors = iter(self.colors)
-        for i, quad in enumerate(self.quads):
-            y = quad.error()
-            er_ax.bar(i,y,width=1,color=next(colors))
+        lines = []
+        for quad in self.quads:
+            error = quad.error()
+            x = len(quad.interval.partitions)
+            line, = er_ax.plot(x,error,'o',color=next(colors))
+            lines.append(line)
 
-    def points(self):
+        return lines
+
+    def get_points(self):
         #plotting the points used for quadrature
-        for quad,ax in zip(self.quads,self.axes):
-            ax.plot(quad.points.x,quad.points.y,".",color="black")
+        points = []
+        for quad,ax in zip(self.quads,self.quad_axes):
+            point, = ax.plot(quad.points.x,quad.points.y,".",color="black")
+            points.append(point)
+        return points
 
-    def quadrature(self, colors: Optional[Iterable[str]] = None):
-        if colors:
-            self.colors = colors
+    def get_shapes(self):
         colors = iter(self.colors)
         artists = []
-        for quad, ax in zip(self.quads, self.axes):
-            artists.extend(quad.graph(ax,next(colors)))
+        for quad, ax in zip(self.quads, self.quad_axes):
+            artists.append(quad.graph(ax,next(colors)))
         return artists
 
     def write(self, filename: str):
         self.fig.tight_layout()
         self.fig.savefig(filename)
+
+class AnimatedGraph(Graph):
+    def __init__(self, frames: Iterable[int], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.frames = frames
+        self.animation = self.animate()
+
+    def anim_init(self):
+        self.error_axes.clear()
+        x_start = min(self.frames) - 1
+        x_end = max(self.frames) + 1
+        self.error_axes.set_xlim(x_start,x_end)
+        min_y = abs(self.calc_min_error())
+        self.error_axes.set_yscale('symlog',linthresh=min_y,)
+
+    def calc_min_error(self) -> int:
+        min_y = abs(self.quads[0].error())
+        max_interval = max(self.frames)
+        for quad in self.quads:
+            quad.interval //= max_interval
+            if abs(quad.error()) <= min_y:
+                min_y = abs(quad.error())
+        return min_y
+
+    def anim_func(self, size:int):
+        for ax in self.quad_axes:
+            ax.clear()
+        for quad in self.quads:
+            quad.interval //= size
+
+        self.error = self.get_error()
+        self.curve = self.get_curve()
+        self.points = self.get_points()
+        self.shapes = self.get_shapes()
+
+        return *self.error, *self.points, *self.shapes
+
+    def animate(self):
+        return FuncAnimation(self.fig, self.anim_func, self.frames, self.anim_init, interval=1000)
+
+    def write(self, filename: str, *args, **kwargs):
+        self.animation.save(filename,*args, **kwargs)
 
 def graph(quad: Quadrature, color = None, filename: str = None):
     graph = Graph(quad)
